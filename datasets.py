d19f36e3a356a02587abcff5c392f79743f091a8
Sergey Romanov <xxsmotur@gmail.com>
2015-06-04 15:40:55 -0700

    add loading imdb dataset

--- datasets.py
+++ datasets.py
@@ -1,8 +1,8 @@
+import imdb
 
-
-def get_imdb(path, n_words, varid_portion=0.1, maxlen=None):
-	""" Get of Large Movie Review datasets """:
-	return imdb.load_data()
+def get_imdb(n_words=100000, varid_portion=0.1, maxlen=None):
+	""" Get of Large Movie Review datasets """
+	return imdb.load_data(path="./imdb.pkl", n_words = n_words, valid_portion=varid_portion, maxlen=maxlen)
 
 def get_wikipedia_data(path):
 	pass
