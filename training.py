b3c07706d5a5f4198994da30d868b8dc7b00a6ad
Sergey Romanov <xxsmotur@gmail.com>
2015-06-04 15:15:23 -0700

    Dirty implementation if rmsprop

--- training.py
+++ training.py
@@ -4,11 +4,17 @@
 
 class RMSProp:
 	""" Implementation of RMSProp for training """
-	def __init__(self):
-		pass
+	def __init__(self, learning_rate=0.001, eps=1e-5, delta=0.99):
+		self.learning_rate = learning_rate
+		self.eps = eps
+		self.delta = delta
+		self.oldparams = None
 
 	def run(self, params, gparams, decay=0.005):
-		return params - gparams * decay
+		value= self.delta * params + (1 - self.delta) * gparams**2
+		result = self.learning_rate * gparams/T.sqrt(result + self.eps)
+		self.oldparams = oldparams
+		return result
 
 
 class SGD:
